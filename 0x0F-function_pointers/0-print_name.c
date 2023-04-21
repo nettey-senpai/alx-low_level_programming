8ab4c4d1f2ed03a2f3775d9c8829e4397792cf27
nettey-senpai <netteyjeff@gmail.com>
2023-04-21 02:27:32 -0700

    task 0 upadate

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,7 +4,7 @@
  * print_name - this prints name using pointer to function
  * @name: the string to add
  * @f: the pointer to function
- * Return: returns nothing
+ * Return: nothing
  **/
 void print_name(char *name, void (*f)(char *))
 {
